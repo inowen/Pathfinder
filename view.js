b27e8fbe9ba1769c932de072fadf2fb963914b04
inowen <inoah@correo.ugr.es>
2021-05-21 21:12:24 +0200

    Separate into files

---
+++ view.js
@@ -0,0 +1,23 @@
+
+canvas = document.getElementById("myCanvas");
+
+window.onresize = function () {
+    canvas = document.getElementById("myCanvas");
+    canvas.height = window.innerHeight * .5;
+    canvas.width = window.innerWidth * .5;
+    ctx = canvas.getContext("2d");
+    ctx.fillStyle = "rgb(0,0,0)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+console.log("The canvas: " + canvas);
+context = canvas.getContext("2d");
+context.fillStyle = "rgb(0,0,180)";
+context.fillRect(0, 0, 256, 256);
+
+// INFO: The image loads asynchronously. So wait until it is loaded before drawing it.
+var img = new Image();
+img.src = "test/hello-hi.jpg";
+img.onload = function () {
+    context.drawImage(img, 10, 10, 100, 100); // x,y,width,height. The image scales.
+}
