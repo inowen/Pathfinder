c04f001585fa3dceb03b74016f8fbb8a070f3808
inowen <inoah@correo.ugr.es>
2021-06-26 18:23:08 +0200

    Add setup.js as a sort of main function

--- setup.js
+++ setup.js
@@ -1,4 +1,66 @@
-// Initialize the Controller singleton
+// DEBUGGING 
 
 
+// Test reading in a map (into a Map object)
+map = new Map2d("country_roads");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Initialize the Controller singleton, map, view, etc.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// END:
 // Set up listeners on html elements to call appropriate controller methods
