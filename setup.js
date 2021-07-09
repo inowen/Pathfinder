88ab574c69b139e9d1c72445ec7189d8c46aa289
inowen <inoah@correo.ugr.es>
2021-07-09 23:16:05 +0200

    Clean up view to reimplement using p5.js

--- setup.js
+++ setup.js
@@ -7,11 +7,7 @@ async function main() {
      */
 
     // Create view, wait for images to load
-    var canvas = document.getElementById('view_canvas');
-    let view = new View(canvas);
-    await view.waitTexturesLoaded();
     
-
 
 
     // Create and initialize model
@@ -22,11 +18,7 @@ async function main() {
 
 
     // DOWN HERE, DEBUGGING
-    let map = new Map2d('country_roads');
-    view.drawMap(map);
-    view.drawPlayer(4, 2, 1);
-    view.drawPlayer(4, 3, 1);
-    view.drawPlayer(4, 4, 1);
+    
     
 }
 
--- view.js
+++ view.js
@@ -1,50 +1,32 @@
 
-
-
-
-// How to wait until these images load?
-/*
-    According to SO, this can be done using Promise. 
-    A function that returns a promise. 
-        The executor sets the onload callback, and inside that callback it uses resolve.
-         (this way the promise is only resolved once the image loads).
-    - Can I then await that from the main thread, or is that one not allowed to wait for stuff?
-*/
-
-
 class View {
-    constructor(canvas) {
-        this.canvas = canvas;
+    constructor(parentDivId) {
 
+        this.p5_object = null; // p5.js object (with setup and draw functions)
 
-        // Load the textures required to paint 
-        // (start loading anyway, later when using them wait for the loading to be completed)
-        this.car_skin = new Image();
-        this.boat_skin = new Image();
-        this.sm_skin = new Image();
-        this.snow = new Image();
-        this.road = new Image();
-        this.floor = new Image();
-        this.cliff = new Image();
-        this.water = new Image();
-        this.obstacle = new Image();
-        this.car = new Image();
-        this.boat = new Image();
-        this.snowmobile = new Image();
+        this.car_skin = null; // textures/character.png
+        this.boat_skin = null; // textures/character.png
+        this.sm_skin = null; // textures/character.png
+        this.snow = null; // textures/snow.png
+        this.road = null; // textures/road.png
+        this.floor = null; // textures/grass.png
+        this.cliff = null; // textures/death.jpg
+        this.water = null; // textures/water.jpg
+        this.obstacle = null; // textures/brick.jpg
+        this.car = null; // textures/garage.jpg
+        this.boat = null; // textures/port.jpg
+        this.snowmobile = null; // textures/snowmobile_garage.jpg
 
-        this.promises = new Array();
-        this.promises.push(this.getLoadPromise(this.car_skin, "textures/character.png"));
-        this.promises.push(this.getLoadPromise(this.boat_skin, "textures/character.png"));
-        this.promises.push(this.getLoadPromise(this.sm_skin, "textures/character.png"));
-        this.promises.push(this.getLoadPromise(this.snow, "textures/snow.png"));
-        this.promises.push(this.getLoadPromise(this.road, "textures/road.png"));
-        this.promises.push(this.getLoadPromise(this.floor, "textures/grass.png"));
-        this.promises.push(this.getLoadPromise(this.cliff, "textures/death.jpg"));
-        this.promises.push(this.getLoadPromise(this.water, "textures/water.jpg"));
-        this.promises.push(this.getLoadPromise(this.obstacle, "textures/brick.jpg"));
-        this.promises.push(this.getLoadPromise(this.car, "textures/garage.jpg"));
-        this.promises.push(this.getLoadPromise(this.boat, "textures/port.jpg"));
-        this.promises.push(this.getLoadPromise(this.snowmobile, "textures/snowmobile_garage.jpg"));
+        let myp5 = new p5( (p) => {
+            this.p5_object = p;
+            p.setup = function() {
+                
+            }
+
+            p.draw = function() {                
+
+            }
+        });
     }
 
 
@@ -67,75 +49,20 @@ class View {
     // (x,y) are the grid coordinates of the parcel where the player is
     // numRowsCols is the size of the map (the number of rows, also the number of colums)
     drawPlayer(parcel_x, parcel_y, orientation, numRowsCols) {
-        let distHorizontal = this.canvas.width / numRowsCols;
-        let distVertical = this.canvas.height / numRowsCols;
-        let x = parcel_x * distHorizontal;
-        let y = parcel_y * distVertical;
-        var ctx = this.canvas.getContext('2d');
-        ctx.fillStyle = "rgb(255,0,0)";
-        // Draw and fill a triangle. Two points on the back edge, one in the center of the opposite side.
-        ctx.beginPath();
-        switch (orientation) {
-            case 0: // Upwards: point bottom left, bottom right, middle up
-                ctx.moveTo(x,y+distVertical);
-                ctx.lineTo(x+distHorizontal, y+distVertical);
-                ctx.lineTo(x+distHorizontal/2, y);
-                break;
-            case 1: // Right: point bottom left, top left, middle right
-            ctx.moveTo(x,y);
-            ctx.lineTo(x,y+distVertical);
-            ctx.lineTo(x+distHorizontal, y+distVertical/2);
-                break;
-            case 2: // Downwards: point top left, top right, middle bottom
-                ctx.moveTo(x,y);
-                ctx.lineTo(x+distHorizontal,y);
-                ctx.lineTo(x+distHorizontal/2, y+distVertical);
-                break;
-            case 3: // Left: point top right, bottom right, middle left
-                ctx.moveTo(x+distHorizontal, y);
-                ctx.lineTo(x+distHorizontal, y+distVertical);
-                ctx.lineTo(x, y+distVertical/2);
-                break;
-        }
-        ctx.closePath();
-        ctx.stroke();
+
     }
 
 
     drawPath(actionsArray) {
 
-
     }
 
     drawGridLines(numRowsCols) {
-        var distHorizontal = this.canvas.width / numRowsCols;
-        var ctx = this.canvas.getContext('2d');
-        ctx.fillStyle = "rgb(0,0,0)";
-        ctx.lineWidth = 1;
-        for (var i=0.5; i<this.canvas.width; i+=distHorizontal) {
-            //ctx.fillRect(i, 0, 1, this.canvas.height);
-            ctx.beginPath();
-            ctx.moveTo(i,0);
-            ctx.lineTo(i,this.canvas.height);
-            ctx.closePath();
-            ctx.stroke();
-        }
-        var distVertical = this.canvas.height / numRowsCols;
-        for (var i=0.5; i<this.canvas.height; i+=distVertical) {
-            ctx.beginPath();
-            ctx.moveTo(0, i);
-            ctx.lineTo(this.canvas.width, i);
-            ctx.closePath();
-            ctx.stroke();
-        }
+
     }
 
     paintParcel(x, y, texChar, numRowCols) {
-        var parcelWidth = this.canvas.width / numRowCols;
-        var parcelHeight = this.canvas.height / numRowCols;
-        var ctx = this.canvas.getContext('2d');
-        var texture = this.getTextureFromChar(texChar);
-        ctx.drawImage(texture, x*parcelWidth+1, y*parcelHeight+1, parcelWidth-1, parcelHeight-1);
+
     }
 
     getTextureFromChar(texChar) {
@@ -163,30 +90,6 @@ class View {
     }
 
 
-    /**
-     * Wait until all the texture images are done loading.
-     * To wait for the textures to load, await his function.
-     * Might throw an exception if the promise is rejected.
-     */
-    async waitTexturesLoaded() {
-        for(var i=0; i<this.promises.length; ++i) {
-            await this.promises[i];
-        }
-    }
- 
-    
-    // Get Promise that resolves as soon as the image loads the given source.
-    getLoadPromise(img, src) {
-        return new Promise((resolve, reject) => {
-            img.onload = () => {
-                resolve("DONE!");
-            };
-            img.onerror = () => {
-                reject("Error");
-            }
-            img.src = src;
-        });
-    }
 
     // Debugging purposes
     printTextures() {
@@ -204,14 +107,5 @@ class View {
         console.log(this.snowmobile);
     }
 
-    printPromises() {
-        console.log(this.promises);
-    }
-
-    testCanvas() {
-        var ctx = this.canvas.getContext('2d');
-        ctx.fillStyle = "rgb(255, 0, 0)";
-        ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-    }
     
 }
