c988e907cf985b099af360213166e207490acaa6
inowen <inoah@correo.ugr.es>
2021-07-16 21:57:58 +0200

    Wrapper for player placement on the map

---
+++ model/Placement.js
@@ -0,0 +1,11 @@
+/**
+ * A Placement refers to the position (x,y) on the map and the orientation.
+ */
+
+class Placement {
+    constructor(x, y, orientation) {
+        this.x = x;
+        this.y = y;
+        this.orientation = orientation;
+    }
+}
