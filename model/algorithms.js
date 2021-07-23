0264a06b2ec7f03ab0ebfedfe6afdbac1adfb0bd
inowen <inoah@correo.ugr.es>
2021-07-23 23:08:15 +0200

    Functions for all pathfinding algorithms

--- model/algorithms.js
+++ model/algorithms.js
@@ -4,6 +4,30 @@ class Algorithms {
     // Add algorithms.
     // Field names ought to be: astar, dfs, bfs, it_deepening, greedy
     constructor() {
-        // Test if I can create a function outside and assign this.field = function;
+        this.astar = PathfindAstar;
+        this.dfs = PathfindDFS;
+        this.bfs = PathfindBFS;
+        this.it_deepening = PathfindIterativeDeepening;
+        this.greedy = PathfindGreedy;
     }
+}
+
+function PathfindAstar(root_node, goal_node, map) {
+
+}
+
+function PathfindDFS(root_node, goal_node, map) {
+
+}
+
+function PathfindBFS(root_node, goal_node, map) {
+
+}
+
+function PathfindIterativeDeepening(root_node, goal_node, map) {
+
+}
+
+function PathfindGreedy(root_node, goal_node, map) {
+
 }
