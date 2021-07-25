a91964eda92862bdc9d5b10d7ceb709d7ecff10a
inowen <inoah@correo.ugr.es>
2021-07-25 19:57:37 +0200

    Linked node list to plan conversion

--- model/algorithms.js
+++ model/algorithms.js
@@ -27,14 +27,12 @@ function PathfindDFS(root_node, goal_node, map) {
 function PathfindBFS(root_node, goal_node, map) {
     var queue = new Array();
     queue.push(root_node);
-    // I need to add a closed set 
-    // ...........................
+    var closedSet = new EqualitySet();
     var path_found = false;
-    var current_node;
+    var current_node = null;
     while(queue.length>0 && !path_found) {
         current_node = queue.shift();
-        // Add current node to the closed set
-        // ..................................
+        closedSet.add(current_node);
         if (current_node.placement.row==goal_node.placement.row
             && current_node.placement.col==goal_node.placement.col
             && current_node.placement.orientation==goal_node.placement.orientation
@@ -44,17 +42,15 @@ function PathfindBFS(root_node, goal_node, map) {
         else {
             var all_children = current_node.generateAllChildren(goal_node, map);ç
             for (var i=0; i<all_children.length; ++i) {
-                // Check if it's in the closed set. If it isn't, add it to the end of the queue.
-                // ............................................................................
+                if (!closedSet.has(all_children[i])) {
+                    closedSet.add(all_children[i]);
+                }
             }
         }
     }
-
-    // Make some kind of function that reconstructs an array of Placement by going up the linked list
-    // starting from a goal node and ending up at the starting node.
-    // (universal function to all algorithms)
+    
     if (path_found) {
-        return linkedListToPlan(current_node);
+        return linkedListToPlan(current_node, root_node);
     }
     return null;
 }
@@ -65,4 +61,20 @@ function PathfindIterativeDeepening(root_node, goal_node, map) {
 
 function PathfindGreedy(root_node, goal_node, map) {
 
+}
+
+
+// ----------------------------------------------------------------------------
+
+function linkedListToPlan(final_node, root_node) {
+    if (final_node == null) {
+        return null;
+    }
+    var plan = new Array();
+    var current_node = final_node;
+    while(current_node != root_node) {
+        plan.unshift(current_node.placement);
+        current_node = current_node.parent;
+    }
+    return plan;
 }
