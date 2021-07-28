7e0b94844ee29a0af5ad10a532fda5b930958d4a
inowen <inoah@correo.ugr.es>
2021-07-28 20:37:22 +0200

    Controller singletonness

--- controller.js
+++ controller.js
@@ -5,10 +5,22 @@
 
 
 class Controller {
+    // Use new to get the instance each time.
+    constructor() {
+        if (Controller.instance) {
+            return Controller.instance;
+        }
+        Controller.instance = this;
+
+        // Initialization
+    }
+
+
     // This is a singleton. The initialization is started from setup.js
 
     // The controller creates the model and the view.
 
     // The setup script attaches event listeners to the html buttons, and those callbacks 
     //      use the controller singleton.
+
 }
