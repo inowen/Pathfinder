8455e8cd142940deebe016c606df2009462fb169
inowen <inoah@correo.ugr.es>
2021-08-05 20:34:15 +0200

    Implement and test PriorityQueue

---
+++ model/PriorityQueue.js
@@ -0,0 +1,99 @@
+
+class PriorityQueue {
+    constructor(comparator = function (a, b) { return a < b; }) {
+        this.arr = [];
+        this.numElements = 0;
+        this.comparator = comparator;
+    }
+
+    enqueue(element) {
+        this.numElements += 1;
+        this.arr[this.numElements] = element;
+        let currentIndex = this.numElements;
+        let parentIndex = (currentIndex - (currentIndex % 2)) / 2;
+        while (currentIndex != 1 && this.comparator(this.arr[currentIndex], this.arr[parentIndex])) {
+            let temp = this.arr[currentIndex];
+            this.arr[currentIndex] = this.arr[parentIndex];
+            this.arr[parentIndex] = temp;
+            currentIndex = parentIndex;
+            parentIndex = (currentIndex - (currentIndex % 2)) / 2;
+        }
+    }
+
+    pop() {
+        let retMe = this.arr[1];
+        if (retMe == undefined) {
+            return undefined;
+        }
+        this.arr[1] = this.arr[this.numElements];
+        this.arr[this.numElements] = undefined;
+        this.numElements -= 1;
+
+        
+        let current = 1;
+        let child1 = 2;
+        let child2 = 3;
+        while ( // While greater than one of the children
+            this.comparator(this.arr[child1], this.arr[current]) ||
+            this.comparator(this.arr[child2], this.arr[current])
+        ) {
+            if (this.arr[child2] < this.arr[child1]) {
+                let temp = this.arr[child2];
+                this.arr[child2] = this.arr[current];
+                this.arr[current] = temp;
+                current = child2;
+            }
+            else {
+                let temp = this.arr[child1];
+                this.arr[child1] = this.arr[current];
+                this.arr[current] = temp;
+                current = child1;
+            }
+
+            child1 = 2 * current;
+            child2 = 2 * current + 1;
+        }
+        return retMe;
+    }
+
+    printContent() {
+        console.log(this.arr);
+        console.log("Current size: " + this.numElements);
+    }
+
+    printTree() {
+        let levelSize = 1;
+        let ind = 1;
+        let output = "";
+        while (ind <= this.numElements) {
+            for (var el = 0; el < levelSize; ++el) {
+                output += (this.arr[ind] + " ");
+                ind += 1;
+            }
+            console.log(output);
+            output = "";
+            levelSize *= 2;
+        }
+    }
+}
+
+
+let pq = new PriorityQueue();
+pq.enqueue(5);
+pq.enqueue(1);
+pq.enqueue(7);
+pq.enqueue(9);
+pq.enqueue(12);
+pq.enqueue(4);
+pq.enqueue(3);
+pq.enqueue(0);
+pq.enqueue(88);
+pq.enqueue(11);
+let extracted = pq.pop();
+console.log("First extracted: " + extracted);
+
+while (extracted != undefined) {
+    console.log(extracted);
+    extracted = pq.pop();
+}
+
