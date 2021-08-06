0e9855c92b190d81e5675dcfa3465a810359c5d4
inowen <inoah@correo.ugr.es>
2021-08-06 11:47:35 +0200

    A* first implementation
    Doesn't work, it obviously doesn't find the best path

--- model/algorithms.js
+++ model/algorithms.js
@@ -15,7 +15,40 @@ class Algorithms {
 }
 
 function PathfindAstar(root_node, goal_node, map) {
-
+    var openSet = new PriorityQueue(function(a, b) {
+        if (a==undefined || b==undefined) {
+            return false;
+        }
+        return a.combined_cost < b.combined_cost;
+    });
+    openSet.enqueue(root_node);
+    var closedSet = new EqualitySet();
+    var path_found = false;
+    var current_node = null;
+    while (openSet.numElements>0 && !path_found) {
+        current_node = openSet.pop();
+        if (closedSet.has(current_node)) {
+            continue;
+        }
+        closedSet.add(current_node);
+        if (current_node.placement.row == goal_node.placement.row
+            && current_node.placement.col == goal_node.placement.col
+        ) {
+            path_found = true;
+        }
+        else {
+            var all_children = current_node.generateAllChildren(goal_node, map);
+            for (var i = 0; i < all_children.length; ++i) {
+                if (!closedSet.has(all_children[i])) {
+                    openSet.enqueue(all_children[i]);
+                }
+            }
+        }
+    }
+    if (path_found) {
+        return linkedListToPlan(current_node, root_node);
+    }
+    return null;
 }
 
 function PathfindDFS(root_node, goal_node, map) {
