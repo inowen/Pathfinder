ab169679d3684a832fbd882987fd5cf795777e5c
inowen <inoah@correo.ugr.es>
2021-08-06 11:05:51 +0200

    Implement greedy pathfinding

--- model/algorithms.js
+++ model/algorithms.js
@@ -78,7 +78,39 @@ function PathfindBFS(root_node, goal_node, map) {
 
 
 function PathfindGreedy(root_node, goal_node, map) {
-
+    var openSet = new PriorityQueue(function(a, b) {
+        if (a==undefined || b==undefined) {
+            return false;
+        }
+        let a_heur = a.combined_cost - a.cost;
+        let b_heur = b.combined_cost - b.cost;
+        return a_heur < b_heur;
+    });
+    openSet.enqueue(root_node);
+    var closedSet = new EqualitySet();
+    var path_found = false;
+    var current_node = null;
+    while (openSet.numElements > 0 && !path_found) {
+        current_node = openSet.pop();
+        closedSet.add(current_node);
+        if (current_node.placement.row == goal_node.placement.row
+            && current_node.placement.col == goal_node.placement.col
+        ) {
+            path_found = true;
+        }
+        else {
+            var all_children = current_node.generateAllChildren(goal_node, map);
+            for (var i = 0; i < all_children.length; ++i) {
+                if (!closedSet.has(all_children[i])) {
+                    openSet.enqueue(all_children[i]);
+                }
+            }
+        }
+    }
+    if (path_found) {
+        return linkedListToPlan(current_node, root_node);
+    }
+    return null;
 }
 
 
